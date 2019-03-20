5f1035a065b3ce2b8ca5084d0d87ce2202f82877
= <7rubengarcia7@gmail.com>
2019-03-20 10:47:22 -0700

    added sass and file loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
+const path = require('path');
+
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: __dirname + "/dist",
-    publicPath: "/",
-    filename: "bundle.js"
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '/dist'),
+    publicPath: '/'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ['babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: ['file-loader']
       }
     ]
   },
   devServer: {
-    contentBase: "./dist"
+    contentBase: './dist'
   }
 };
